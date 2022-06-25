ba21dc93f9b0ecf49d971590076fe6e21c1e7750
s41m4 <87184314+S411m4@users.noreply.github.com>
2022-06-26 01:28:49 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,10 +1,28 @@
 #To do
 #2's complement range
-#binary_to_hex conversions error
 
 
 
-from numpy import real
+
+base_64_binary_dict = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9, 'K':10, 'L':11, 'M':12, 'N':13,
+                       'O':14, 'P':15, 'Q':16, 'R':17, 'S':18, 'T':19, 'U':20, 'V':21, 'W':22, 'X':23, 'Y':24, 'Z':25,
+                       'a':26, 'b':27, 'c':28, 'd':29, 'e':30, 'f':31, 'g':32, 'h':33, 'i':34, 'j':35, 'k':36, 'l':37,
+                       'm':38, 'n':39, 'o':40, 'p':41, 'q':42, 'r':43, 's':44, 't':45, 'u':46, 'v':47, 'w':48, 'x':49,
+                       'y':50, 'z':51, '0':52, '1':53, '2':54, '3':55, '4':56, '5':57, '6':58, '7':59, '8':60, '9':61,
+                       '+':62, '/':63, '=':000000}
+
+
+
+base_64_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L',
+                12: 'M', 13: 'N',
+                14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y',
+                25: 'Z', 26: 'a',
+                27: 'b', 28: 'c', 29: 'd', 30: 'e', 31: 'f', 32: 'g', 33: 'h', 34: 'i', 35: 'j', 36: 'k', 37: 'l',
+                38: 'm', 39: 'n',
+                40: 'o', 41: 'p', 42: 'q', 43: 'r', 44: 's', 45: 't', 46: 'u', 47: 'v', 48: 'w', 49: 'x', 50: 'y',
+                51: 'z', 52: '0',
+                53: '1', 54: '2', 55: '3', 56: '4', 57: '5', 58: '6', 59: '7', 60: '8', 61: '9', 62: '+', 63: '/'
+                }
 
 
 unreadable_ASCII_dict = {1: '~START OF HEADING~',
@@ -205,34 +223,58 @@ def string_to_binary(sentence):
 def binary_to_string(binary):
     #input 7-bit or 8-bit binary (string) -> output equivalent string
 
-    index = 0
-    sentence = []
+    try:
+        index = 0
+        sentence = []
 
-    if len(binary) % 7 == 0:
+        if len(binary) % 7 == 0:
+        
+            while(index  < len(binary) - 1):    #21  0 - 6, 7 - 13 , 14 - 20
+                letter = binary[index: index + 7]
+                sentence.append(binary_to_char(letter))
+                index += 7
+
+            return sentence
+
+        elif len(binary) % 8 == 0:
+            while(index < len(binary) - 1):    #24  0 - 7, 8 - 15 , 16 - 23
+                letter = binary[index: index + 8]  #we don't counter last boundary with us
+                sentence.append(binary_to_char(letter))
+                index += 8
+
+            return sentence
+
+        else:
+            return '#'
+
+    except:
+        return '#'
     
-        while(index + 6 < len(binary) - 1):    #21  0 - 6, 7 - 13 , 14 - 20
-            letter = binary[index: index + 7]
-            sentence.append(binary_to_char(letter))
-            index += 7
+'''
+def binary_to_base64(binary):
+    #input binary -> output base64
+    try:    
+        index = 0
+        sentence = []
 
-        return sentence
+        if len(sentence) % 6 == 0:
+            
+            while(index  < len(binary) - 1):    
+                letter = binary[index: index + 6] #we don't take last boundary
+                sentence.append(binary_to_base64[binary_to_no(letter)])
+                index += 6  
 
-    elif len(binary) % 8 == 0:
-        while(index < len(binary) - 1):    #24  0 - 7, 8 - 15 , 16 - 23
-            letter = binary[index: index + 8]  #we don't counter last boundary with us
-            sentence.append(binary_to_char(letter))
-            index += 8
+            return sentence
 
-        return sentence
+        else:
+            return '#'
 
-    else:
+    except:
         return '#'
 
+'''
 
-if __name__ == "__main__" :
-    print(string_to_binary('hello'))
-    
-    print(binary_to_string('0110100001100101011011000110110001101111'))
-    
+
+
     
     
